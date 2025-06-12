aad9ea0d3bdb989d0cd82d155a180505646de4f5
AbuBakar223200 <mbakar223200@bscse.uiu.ac.bd>
2025-06-12 19:09:27 +0600

    Login page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { LogIn } from "lucide-react"
+import React from "react";
+import Login from "./pages/authentication/Login";
 
 function App() {
 
   return (
     <>
+      <Login/>
       
     </>
   )
